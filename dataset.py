5a3859524258747c3d935cf0e8bde5502aac1eab
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2024-07-05 16:23:58 -0300

    processing dataset

--- dataset.py
+++ dataset.py
@@ -1,17 +1,26 @@
 import torch
-from torch_geometric.data import InMemoryDataset
+from torch_geometric.data import InMemoryDataset, Data
+from transformers import BertTokenizer
 
 import pandas as pd 
 import numpy as np
 
-class Dataset(InMemoryDataset):
+import os.path 
+
+class RelationsDS(InMemoryDataset):
     edge2id = {
         'Hypernyms': 0,
         'Holonyms': 1
     }
 
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self, root, tokenizer=None, transform=None, pre_transform=None, pre_filter=None):
+        if tokenizer is None:
+            self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        else:
+            self.tokenizer = tokenizer
+        
         super().__init__(root, transform, pre_transform, pre_filter)
+
         self.load(self.processed_paths[0])
 
     @property
@@ -39,7 +48,7 @@ class Dataset(InMemoryDataset):
         self.save(data_list, self.processed_paths[0])
     
     def process_csv(self):
-        path = self.root + self.processed_file_names()[0]
+        path = os.path.join(self.root, self.raw_file_names[0])
         df = pd.read_csv(path)
 
         entities = pd.concat([df['ID_Synset'], 
@@ -54,4 +63,19 @@ class Dataset(InMemoryDataset):
 
         edge_type = df['Relacao'].map(self.edge2id).to_numpy()
 
-        self.num_nodes = entities.shape[-1]
+        tokens = self.tokenizer(list(definitions),
+                                padding=True, 
+                                return_tensors='pt')
+        token_ids = tokens.input_ids
+        token_mask = tokens.attention_mask
+        token_type_ids = tokens.token_type_ids 
+
+        data = Data()
+        data.edge_index = edge_index
+        data.edge_type = edge_type
+        data.token_ids = token_ids
+        data.token_mask = token_mask
+        data.token_type_ids = token_type_ids
+
+        return [data]
+
