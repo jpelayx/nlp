fixed dumbass data leakage

dataset.py
@@ -24,7 +24,6 @@ class RelationsDS(InMemoryDataset):
         
         super().__init__(root, transform, pre_transform, pre_filter)
         self.load(self.processed_paths[0])
-        self.num_nodes = self[0].token_ids.shape[0]
 
     @property
     def raw_file_names(self):
@@ -78,14 +77,46 @@ class RelationsDS(InMemoryDataset):
         token_mask = tokens.attention_mask
         token_type_ids = tokens.token_type_ids 
 
-        data = Data()
-        data.edge_index = torch.tensor(edge_index, dtype=torch.long)
-        data.edge_attr = torch.tensor(edge_attr, dtype=torch.long)
-        data.token_ids = token_ids
-        data.token_mask = token_mask
-        data.token_type_ids = token_type_ids
+        device = 'cuda'
+        data = Data().to(device)
+        data.num_nodes = ids.shape[0]
+        data.edge_index = torch.tensor(edge_index, dtype=torch.long, device=device)
+        data.edge_attr = torch.tensor(edge_attr, dtype=torch.long, device=device)
+        data.token_ids = token_ids.to(device)
+        data.token_mask = token_mask.to(device)
+        data.token_type_ids = token_type_ids.to(device)
 
-        return [data]
+        data = self._create_splits(data, device)
+        data = self._add_self_loops(data, data.num_nodes, device)
+
+        return [data.to('cpu')]
+    
+    def _create_splits(self, data, device):
+        src, tar, neg = structured_negative_sampling(data.edge_index,
+                                                     contains_neg_self_loops=False)
+        data.pos_samples = torch.stack([src, tar])
+        data.neg_samples = torch.stack([src, neg])
+    
+        mask = torch.arange(src.shape[0], device='cpu')
+        labels = data.edge_attr.to('cpu')
+        train_mask, test_mask = train_test_split(mask, test_size=0.2, stratify=labels)
+        train_mask, val_mask = train_test_split(train_mask, test_size=0.2, stratify=labels[train_mask])
+        data.train_mask = train_mask.to(device)
+        data.val_mask = val_mask.to(device)
+        data.test_mask = test_mask.to(device)
+        data.y = labels.to(device)
+        data.edge_index = data.edge_index[:,data.train_mask]
+        data.edge_attr = data.edge_attr[data.train_mask]
+        return data
+    
+    def _add_self_loops(self, data, num_nodes, device):
+        edge_index_i = torch.cat([data.edge_index[0], torch.arange(num_nodes, dtype=torch.int64, device=device)]) 
+        edge_index_j = torch.cat([data.edge_index[1], torch.arange(num_nodes, dtype=torch.int64, device=device)]) 
+        data.edge_index = torch.stack([edge_index_i, edge_index_j])
+        
+        data.edge_attr = torch.cat([data.edge_attr, torch.zeros(num_nodes, dtype=torch.long, device=device)])
+        
+        return data
 
     def _compose_definition(self, arg):
         id, definition = arg

train.py
@@ -154,9 +154,9 @@ if __name__ == '__main__':
     node_embedder = NodeEmbedder(input_dim=768,
                                  hidden_dim=64,
                                  output_dim=128,
-                                 num_heads=4,
-                                 num_layers=1, 
-                                 dropout=0.2)
+                                 num_heads=2,
+                                 num_layers=2, 
+                                 dropout=0.3)
     link_pred = LinkPredictor(input_dim=128, 
                               hidden_dim=256,
                               output_dim=3,
@@ -167,8 +167,8 @@ if __name__ == '__main__':
         model.load_state_dict(torch.load('.best_model.pth'))    
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)  
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150], gamma=0.5) 
-    # scheduler = None
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[200], gamma=0.5) 
+    scheduler = None
 
     encoding_path = './data/input_encoding.pt'
     if not os.path.exists(encoding_path):
@@ -181,8 +181,6 @@ if __name__ == '__main__':
         node_encodings = node_embedder.load_input_encodings(encoding_path).to(device)
         print('Loaded pre-computed BERT encodings.')
     g.x = node_encodings 
-    
-    g = split_data_stratified(g, data.num_nodes) # create neg samples and train/val/test splits 
     g.edge_attr = F.one_hot(g.edge_attr).float()
 
     train_epochs = args.epochs