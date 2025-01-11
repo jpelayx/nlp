24619c6677745bfc72230bb4d75f8846abb2c56d
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2025-01-11 20:25:26 -0300

    adjusted model and sample training procedure

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
-import torch 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch.nn import Module, ModuleList, Sequential
+from torch.nn import Module, ModuleList, Sequential, Conv1d
 from torch.utils.data import DataLoader
 from torch_geometric.nn import GCNConv, GATv2Conv, BatchNorm
 from transformers import BertModel
@@ -11,104 +11,74 @@ import numpy as np
 
 import os.path
 
+
 class NodeEmbedder(Module):
-    def __init__(self,
-                 input_dim,
-                 hidden_dim, 
-                 output_dim, 
-                 num_layers, 
-                 num_heads, 
-                 dropout=None,
-                 use_precomputed_encodings=True,
-                 encoder=None
+    def __init__(
+        self,
+        input_dim,
+        hidden_dim,
+        output_dim,
+        edge_dim,
+        num_layers,
+        num_heads,
+        dropout=None,
     ) -> None:
         super().__init__()
 
-        if num_layers == 0:
-            self._fake = True
-            return 
-        else:
-            self._fake = False
-
-        if not use_precomputed_encodings:
-            if encoder is None:
-                self.encoder = BertModel.from_pretrained('bert-base-uncased')
-            for param in self.encoder.parameters():
-                param.requires_grad = False
-        self._default_encoding_path = 'data/input_encoding.pt'
+        self._fake = num_layers == 0
+        if self._fake:
+            return
 
         self.dropout = dropout
         self.num_layers = num_layers
+
         self.norm_layers = ModuleList()
         self.norm_layers.append(BatchNorm(input_dim))
-        self.conv_layers = ModuleList()
-        self.conv_layers.append(GATv2Conv(input_dim, 
-                                          hidden_dim, 
-                                          num_heads,
-                                          edge_dim=3,
-                                          add_self_loops=False))
-        self.lin_layers = ModuleList()
-        self.lin_layers.append(nn.Linear(hidden_dim*num_heads, 
-                                         hidden_dim*num_heads))
-        for _ in range(num_layers-1):
-            self.norm_layers.append(BatchNorm(hidden_dim*num_heads))
-            self.conv_layers.append(GATv2Conv(hidden_dim*num_heads,
-                                              hidden_dim,
-                                              num_heads,
-                                              edge_dim=3,
-                                              add_self_loops=False))
-            self.lin_layers.append(nn.Linear(hidden_dim*num_heads, 
-                                             hidden_dim*num_heads))
 
+        self.conv_layers = ModuleList()
+        self.conv_layers.append(
+            GATv2Conv(
+                input_dim,
+                hidden_dim,
+                num_heads,
+                edge_dim=edge_dim,
+                add_self_loops=False,
+            )
+        )
+
+        self.lin_layers = ModuleList()
+        self.lin_layers.append(
+            nn.Linear(hidden_dim * num_heads, hidden_dim * num_heads)
+        )
+
+        for _ in range(num_layers - 1):
+            self.norm_layers.append(BatchNorm(hidden_dim * num_heads))
+            self.conv_layers.append(
+                GATv2Conv(
+                    hidden_dim * num_heads,
+                    hidden_dim,
+                    num_heads,
+                    edge_dim=edge_dim,
+                    add_self_loops=False,
+                )
+            )
+            self.lin_layers.append(
+                nn.Linear(hidden_dim * num_heads, hidden_dim * num_heads)
+            )
 
         self.out = Sequential(
-            nn.Linear(hidden_dim*num_heads, hidden_dim),
-            nn.Linear(hidden_dim, output_dim))
+            nn.Linear(hidden_dim * num_heads, hidden_dim),
+            nn.Linear(hidden_dim, output_dim),
+        )
 
-    def encode_inputs(self, tokens:torch.Tensor, batch_size=1024, encoding_dim=768, verbose=False):
-        token_loader = DataLoader(tokens, batch_size)
-        num_batches = len(token_loader)
-        encodings = None
-        for batch_idx, token_batch in enumerate(token_loader):
-            if verbose and (batch_idx % 10*batch_size == 0):
-                print(f'Batch {batch_idx}/{num_batches}')
-            batch_encoding = self.encoder(token_batch[:,0,:],
-                                          token_batch[:,1,:],
-                                          token_batch[:,2,:]).last_hidden_state[:,0,:]
-            if encodings is None: 
-                encodings = batch_encoding
-            else:
-                encodings = torch.concat([encodings, batch_encoding])
-        return encodings
-    
-    def save_input_encodings(self, encodings, path=None):
-        if path is None:
-            path = self._default_encoding_path
-        torch.save(encodings, path)
-
-    def load_input_encodings(self, path=None):
-        if path is None:
-            path = self._default_encoding_path
-        assert os.path.exists(path)
-
-        encodings = torch.load(path)
-        return encodings
-
-    def forward(self, 
-                x:torch.Tensor, 
-                edge_index:torch.Tensor, 
-                edge_attr:torch.Tensor,
-                encode_tokens=False
+    def forward(
+        self,
+        x: torch.Tensor,
+        edge_index: torch.Tensor,
+        edge_attr: torch.Tensor,
     ) -> torch.Tensor:
-        if encode_tokens:
-            assert x.dim() == 3
-            assert x.shape[1] == 3
-
-            x = self.encode_inputs(x)
-            self.save_input_encodings(x)
         if self._fake:
             return x
-        x_0 = x        
         for l in range(self.num_layers):
             x = self.norm_layers[l](x)
             x = self.conv_layers[l](x, edge_index, edge_attr)
@@ -116,38 +86,51 @@ class NodeEmbedder(Module):
             x = F.relu(x)
             if not self.dropout is None:
                 x = F.dropout(x, p=self.dropout, training=self.training)
-        x = x_0 + x
-        
         return self.out(x)
 
+
 class LinkPredictor(Module):
-    def __init__(self, 
-                 input_dim, 
-                 hidden_dim, 
-                 output_dim, 
-                 num_layers, 
-                 dropout=None
+    def __init__(
+        self, input_dim, hidden_dim, output_dim, edge_dim, num_layers, dropout=None
     ) -> None:
         super().__init__()
         self.dropout = dropout
         self.num_layers = num_layers
         try:
             _ = (hd for hd in hidden_dim)
-        except:
-            hidden_dim = torch.tensor([hidden_dim]).repeat(num_layers-1)
-        self.normalization_layers = ModuleList()
-        self.normalization_layers.append(BatchNorm(input_dim*2))
-        self.linear_layers = ModuleList()
-        self.linear_layers.append(nn.Linear(input_dim*2, hidden_dim[0]))
-        for l in range(num_layers - 2):
-            self.normalization_layers.append(BatchNorm(hidden_dim[l]))
-            self.linear_layers.append(nn.Linear(hidden_dim[l], hidden_dim[l+1]))
-        self.linear_layers.append(nn.Linear(hidden_dim[-1], output_dim))
-        self.normalization_layers.append(BatchNorm(hidden_dim[-1]))
+            assert len(hidden_dim) == num_layers
+        except AssertionError:
+            raise ValueError("hidden_dim must be an iterable of length num_layers")
+        except TypeError:
+            hidden_dim = torch.tensor([hidden_dim]).repeat(num_layers)
 
-    def forward(self, x_i, x_j):
-        x = torch.cat([x_i, x_j], dim=1)
-        for l in range(self.num_layers-1):
+        self.r_projection = nn.Linear(edge_dim, input_dim)
+
+        self.conv = nn.Conv1d(input_dim, input_dim, kernel_size=2)
+
+        self.normalization_layers = ModuleList()
+        self.normalization_layers.append(BatchNorm(input_dim * 2))
+
+        self.linear_layers = ModuleList()
+        self.linear_layers.append(nn.Linear(input_dim * 2, hidden_dim[1]))
+
+        for l in range(1, num_layers - 1):
+            self.normalization_layers.append(BatchNorm(hidden_dim[l - 1]))
+            self.linear_layers.append(nn.Linear(hidden_dim[l - 1], hidden_dim[l]))
+
+        self.normalization_layers.append(BatchNorm(hidden_dim[-1]))
+        self.linear_layers.append(nn.Linear(hidden_dim[-1], output_dim))
+
+    def forward(self, xs, xr, xo):
+        xr = self.r_projection(xr)
+        xr = F.relu(xr)
+
+        x = self.conv(torch.stack([xs, xo], dim=-1)).squeeze(-1)
+        x = F.relu(x)
+
+        x = torch.cat([x, xo], dim=1)
+
+        for l in range(self.num_layers - 1):
             x = self.normalization_layers[l](x)
             x = self.linear_layers[l](x)
             x = F.relu(x)
@@ -155,16 +138,22 @@ class LinkPredictor(Module):
                 x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.normalization_layers[-1](x)
         x = self.linear_layers[-1](x)
-        # return torch.sigmoid(x)
+
         return x
+
 
 class Model(Module):
     def __init__(self, node_embedder, link_predictor) -> None:
         super().__init__()
         self.node_embedder = node_embedder
         self.link_predictor = link_predictor
-    def forward(self, x, edge_index, edge_attr, target_links):
+
+    def forward(self, x, edge_index, edge_attr, target_edges, target_edge_attrs):
         emb = self.node_embedder(x, edge_index, edge_attr)
-        preds = self.link_predictor(emb[target_links[0]],
-                                    emb[target_links[1]])
+
+        xs = emb[target_edges[0]]
+        xo = emb[target_edges[1]]
+        xr = target_edge_attrs
+        preds = self.link_predictor(xs, xr, xo)
+
         return preds
---
+++ run.py
@@ -0,0 +1,76 @@
+from dataset import WN18RR
+from model import NodeEmbedder, LinkPredictor, Model
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
+from torch_geometric.utils import structured_negative_sampling, remove_self_loops
+
+
+def train(model, optimizer, g, targets, batch_size=None, scheduler=None):
+    model.train()
+    optimizer.zero_grad()
+
+    batch_size = batch_size if batch_size else len(targets)
+    link_loader = DataLoader(targets, batch_size=batch_size, shuffle=True)
+
+    total_loss = 0
+    for edges, attrs, labels in link_loader:
+        preds = model(g.x, g.edge_index, g.edge_attr, edges.T, attrs)
+        loss = F.binary_cross_entropy_with_logits(preds.flatten(), labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += torch.sum(loss)
+    if scheduler:
+        scheduler.step()
+    return total_loss / len(targets)
+
+
+def get_train_targets(g):
+    target_edges, target_attrs = remove_self_loops(g.edge_index, g.edge_attr)
+    target_labels = torch.ones(target_edges.shape[1], dtype=torch.float)
+
+    i, _, k = structured_negative_sampling(
+        target_edges, g.num_nodes, contains_neg_self_loops=False
+    )
+    negative_edges = torch.stack([i, k])
+
+    target_edges = torch.cat([target_edges, negative_edges], dim=1)
+    target_attrs = target_attrs.repeat(2, 1)
+    target_labels = torch.cat([target_labels, torch.zeros_like(target_labels)])
+
+    targets = TensorDataset(target_edges.T, target_attrs, target_labels)
+    return targets
+
+
+if __name__ == "__main__":
+    import time
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    node_embedder = NodeEmbedder(
+        input_dim=768,
+        hidden_dim=256,
+        output_dim=256,
+        edge_dim=12,
+        num_heads=1,
+        num_layers=2,
+    ).to(device)
+
+    link_pred = LinkPredictor(
+        input_dim=256, hidden_dim=128, output_dim=1, edge_dim=12, num_layers=3
+    ).to(device)
+
+    model = Model(node_embedder, link_pred)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    g = WN18RR(root="data/wn18rr", split="train")[0].to(device)
+    g.edge_attr = F.one_hot(g.edge_attr, 12).float().squeeze(1)
+    train_targets = get_train_targets(g)
+
+    for epoch in range(10):
+        t0 = time.time()
+        loss = train(model, optimizer, g, train_targets, batch_size=1024)
+        tf = time.time()
+        print(f"Epoch {epoch} - Loss: {loss} ({tf - t0:.2f}s)")
